ed134ed71a1812aa0a3df97e53606a0974264847
Mosudi I. O. <imosudi@gmail.com>
2026-01-29 05:31:07 +0100

    Sundry Updates

--- app/routes/solutions.py
+++ app/routes/solutions.py
@@ -15,11 +15,33 @@ def index():
 def infrastructure():
     return render_template("solutions/infrastructure.html")
 
+@solutions_bp.route("/infrastructure/datacentre_infra/", endpoint="datacentre_infra")
+def datacentre_infra():
+    return render_template("solutions/infrastructures/datacentre.html")
+
+@solutions_bp.route("/infrastructure/server_infra/", endpoint="server_infra")
+def server_infra():
+    return render_template("solutions/infrastructures/servers.html")
+
+@solutions_bp.route("/infrastructure/monitoring_uptime/", endpoint="monitoring_uptime")
+def monitoring_uptime():
+    return render_template("solutions/infrastructures/monitoring_uptime.html")
 
 @solutions_bp.route("/networking/", endpoint="networking")
 def networking():
     return render_template("solutions/networking.html")
 
+@solutions_bp.route("/networking/enterpise_networks/", endpoint="Enterprise_Networking")
+def enterpise_networks():
+    return render_template("solutions/networks/enterprise_lan_wan.html")
+
+@solutions_bp.route("/networking/fibre_wireless/", endpoint="Fibre_Wireless")
+def fibre_wireless():
+    return render_template("solutions/networks/fibre_wireless.html")
+
+@solutions_bp.route("/networking/5g/", endpoint="5G")
+def fiveg():
+    return render_template("solutions/networks/5g.html")
 
 @solutions_bp.route("/cloud/", endpoint="cloud")
 def cloud():
